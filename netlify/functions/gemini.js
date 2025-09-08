2192ecd9d5d7cee1ae7757fa2b8b106ce657027c
Rorisang-Mathiba <rmathiba71@gmail.com>
2025-09-08 13:38:05 +0200

    updated gemini

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -3,12 +3,10 @@ export async function handler(event) {
     const body = JSON.parse(event.body);
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           contents: [{ role: "user", parts: [{ text: body.prompt }] }],
         }),
@@ -16,7 +14,6 @@ export async function handler(event) {
     );
 
     const data = await response.json();
-
     return {
       statusCode: 200,
       body: JSON.stringify(data),
